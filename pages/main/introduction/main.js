1673d7174f1b40b4b224dc33c5055b2fad1368f1
Philippe <philippe.idalgoprestes@gmail.com>
2025-12-21 12:06:00 -0300

    m

--- pages/main/introduction/main.js
+++ pages/main/introduction/main.js
@@ -22,16 +22,17 @@ export default function introduction(){
         async function a(){
             let viewport = window.innerHeight
             let scrolled = window.scrollY
+            let d = window.location.replaceAll("/404.html","")
 
             let background = introduction.children[0]
-            if(scrolled < viewport*3 && background.src != "http://127.0.0.1:5500/assets/zootopia.jpg"){
+            if(scrolled < viewport*3 && background.src != `${d}/assets/zootopia.jpg`){
                 background.style.opacity = 0
                 await new Promise(resolve => setTimeout(resolve, 501))
                 background.src = "./assets/zootopia.jpg"
                 await new Promise(resolve => setTimeout(resolve, 1))
                 background.style.opacity = 0.5
             }
-            else if(scrolled >= viewport*3 && background.src != "http://127.0.0.1:5500/assets/dengo.png"){
+            else if(scrolled >= viewport*3 && background.src != `${d}/assets/dengo.png`){
                 background.style.opacity = 0
                 await new Promise(resolve => setTimeout(resolve, 501))
                 background.src = "./assets/dengo.png"
